b635df8292a94ceb2b1119ab3655c9b814ce49ec
Juan Carlos Garces Sierra <juancgarcess@gmail.com>
2024-09-18 21:37:13 -0500

    Edicion de codigo y nuevas carpetas
    Se agrego código a catalogo_peliculas.py; Se creo la carpeta img y se agrego el logo; se creo la carpeta cliente y el init.py el gui_app.py

--- catalogo-peliculas/catalogo_peliculas.py
+++ catalogo-peliculas/catalogo_peliculas.py
@@ -2,6 +2,14 @@ import tkinter as tk
 
 def main():
     root = tk.Tk()
-
+    root.title("Catalogo de peliculas")
+    #root.iconbitmap('img\cp-logo.ico') #me arroja error
+    root.resizable(0,0)
+    
+    frame = tk.Frame(root)
+    frame.pack()
+    frame.config(width=480, height=320, bg = 'green')
+    root.mainloop()
 if __name__ == '__main__':
+    
     main()
