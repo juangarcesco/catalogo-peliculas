c9d8cb53793dede8442484c299f4d1126855b591
Juan Carlos Garces Sierra <juancgarcess@gmail.com>
2024-09-20 00:16:55 -0500

    Se ordenan carpetas, se edita gui_app, se corrigue el icono por el slash

--- catalogo-peliculas/catalogo_peliculas.py
+++ catalogo-peliculas/catalogo_peliculas.py
@@ -1,15 +1,17 @@
 import tkinter as tk
+from client.gui_app import Frame
 
 def main():
     root = tk.Tk()
     root.title("Catalogo de peliculas")
-    #root.iconbitmap('img\cp-logo.ico') #me arroja error
+    root.iconbitmap('img/cp-logo.ico') 
     root.resizable(0,0)
     
-    frame = tk.Frame(root)
-    frame.pack()
-    frame.config(width=480, height=320, bg = 'green')
+
+    app = Frame(root = root)
+    
     root.mainloop()
+
 if __name__ == '__main__':
     
     main()
