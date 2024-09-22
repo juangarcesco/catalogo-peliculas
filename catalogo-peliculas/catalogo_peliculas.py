9515347fb02f91af3ede921d71e1213f3a57b3ee
Juan Carlos Garces Sierra <juancgarcess@gmail.com>
2024-09-21 23:51:32 -0500

    Se agregan los menus y campos

--- catalogo-peliculas/catalogo_peliculas.py
+++ catalogo-peliculas/catalogo_peliculas.py
@@ -1,11 +1,13 @@
 import tkinter as tk
-from client.gui_app import Frame
+from client.gui_app import Frame, barra_menu
 
 def main():
     root = tk.Tk()
     root.title("Catalogo de peliculas")
     root.iconbitmap('img/cp-logo.ico') 
     root.resizable(0,0)
+    
+    barra_menu(root)
     
 
     app = Frame(root = root)
--- catalogo-peliculas/client/gui_app.py
+++ catalogo-peliculas/client/gui_app.py
@@ -1,8 +1,41 @@
 import tkinter as tk
 
+def barra_menu(root):
+    barra_menu = tk.Menu(root)
+    root.config(menu = barra_menu, width =300, height = 300)
+
+    menu_inicio = tk.Menu(barra_menu, tearoff = 0)
+    barra_menu.add_cascade(label = 'Inicio', menu =menu_inicio)
+
+    menu_inicio.add_command(label = 'Crear un registro en DB')
+    menu_inicio.add_command(label = 'Eliminar un registro en DB')
+    menu_inicio.add_command(label = 'Salir', command = root.destroy)
+
+    barra_menu.add_cascade(label='Consultas')
+    barra_menu.add_cascade(label='Configuracion')
+    barra_menu.add_cascade(label='Ayuda')
+
+
 class Frame(tk.Frame):
     def __init__(self, root = None):
         super().__init__(root, width=480, height=320)
         self.root = root
         self.pack()
-        self.config(bg='green')
+        #self.config(bg='green')
+
+        self.campos_pelicula()
+
+    def campos_pelicula(self):
+        # Labels de cada campo
+        self.label_nombre = tk.Label(self, text = 'Nombre: ')
+        self.label_nombre.config(font = ('Arial', 12, 'bold'))
+        self.label_nombre.grid(row = 0, column = 0, padx = 10, pady = 10)
+
+        self.label_duracion = tk.Label(self, text='Duración: ')
+        self.label_duracion.config(font=('Arial', 12, 'bold'))
+        self.label_duracion.grid(row=1, column=0, padx=10, pady=10)
+
+        self.label_genero = tk.Label(self, text='Genero: ')
+        self.label_genero.config(font=('Arial', 12, 'bold'))
+        self.label_genero.grid(row=2, column=0, padx=10, pady=10)
+
